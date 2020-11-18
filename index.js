9d1fb4434249e1f60cc36d5d171044c2e67e7d55
Javier Gonzalez <57018935+javierjose25@users.noreply.github.com>
2020-11-18 09:58:26 -0500

    solobackend2

--- index.js
+++ index.js
@@ -4,15 +4,20 @@ const users = require('./routes/users');
 
 const app = express();
 
+app.set('port', process.env.PORT || 3001);
 
 app.use(express.json());
 
-//app.use(cors({origin: '*'}));
-app.use(cors({origin: 'https://kuepj-3000.sse.codesandbox.io'}));
+app.use(cors({origin: '*'}));
+//app.use(cors({origin: 'https://kuepj-3000.sse.codesandbox.io'}));
+
+app.get('/',(req,res)=>{
+  res.send('hi');
+});
 
 // Routes//
 app.use('/api',users);
 
-app.listen(3001, () => {
+app.listen(app.get('port'), () => {
   console.log(`running server port 3001`);
 });
